ddec56a9bfce2530fd9ec191b170fdaa409d5f28
Danii47 <uranux1@gmail.com>
2025-08-04 01:37:03 +0200

    refactor: import paths and simplifies profile page

--- src/app/(private)/feed/page.tsx
+++ src/app/(private)/feed/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useRequireAuth } from "@/hooks/useRequiredAuth"
-import { useAuth } from "../../../context/AuthContext"
+import { useAuth } from "@/context/AuthContext"
 import Image from "next/image"
 
 export default function Dashboard() {
--- src/app/(private)/profile/page.tsx
+++ src/app/(private)/profile/page.tsx
@@ -1,17 +1,15 @@
 "use client"
 
-import { useRequireAuth } from "../../../hooks/useRequiredAuth"
+import { useRequireAuth } from "@/hooks/useRequiredAuth"
 
 export default function Dashboard() {
-  const { user, loading } = useRequireAuth()
+  const { /*user,*/ loading } = useRequireAuth()
 
   if (loading) return <p>Cargando...</p>
 
   return (
     <div>
-      <h1>Bienvenido {user?.id}</h1>
- 
-      <p>Contenido privado PROFILE</p>
+      HOLA
     </div>
   )
 }
